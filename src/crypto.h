1f52a571ca52386f7c2d39589df94c944cda9e5a
Eric Lagergren <elagergren@spideroak-inc.com>
2020-07-07 12:37:00 -0700

    add SQLCIPHER_CRYPTO_NONE
    Signed-off-by: Eric Lagergren <elagergren@spideroak-inc.com>

--- src/crypto.h
+++ src/crypto.h
@@ -51,7 +51,8 @@ void sqlite3pager_error(Pager*, int);
 void sqlite3pager_reset(Pager *pPager);
 /* end extensions defined in pager.c */
 
-#if   !defined (SQLCIPHER_CRYPTO_WOLFCRYPT)   \
+#if   !defined (SQLCIPHER_CRYPTO_NONE)        \
+   && !defined (SQLCIPHER_CRYPTO_WOLFCRYPT)   \
    && !defined (SQLCIPHER_CRYPTO_BCRYPT)      \
    && !defined (SQLCIPHER_CRYPTO_CC)          \
    && !defined (SQLCIPHER_CRYPTO_LIBTOMCRYPT) \
@@ -61,11 +62,11 @@ void sqlite3pager_reset(Pager *pPager);
 #endif
 
 #if defined(SQLCIPHER_CRYPTO_DEFAULT)
-	/* Default to CommonCrypto on macOS, etc. */
+	/* Default to CommonCrypto on macOS, iOS, etc. */
 	#if defined(__APPLE__)
 		#define SQLCIPHER_CRYPTO_CC
-	/* Default to OpenSSL on Linux. */
-	#elif defined(__linux__)
+	/* Default to OpenSSL on Linux, Android. */
+	#elif (defined(__linux__) || defined(__ANDROID__))
 		#define SQLCIPHER_CRYPTO_OPENSSL
 	/* Default to BCrypt (CNG) on Windows. */
 	#elif (defined(_WIN64) || defined(_WIN32))
